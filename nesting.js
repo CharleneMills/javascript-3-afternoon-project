282ae63707dbb6c35907c3e6f34458c02fbea7bf
CharleneMills <charleneskater@gmail.com>
2020-05-02 12:45:47 -0600

    NESTING IS FINISHED -- YAY!

--- nesting.js
+++ nesting.js
@@ -50,7 +50,20 @@ var employees = [
     3. Return the updated employee array.
 */
 
-//Code Here
+const employeeUpdater = function (){
+  
+  for (let i = 0; i < employees.length; i++){
+    if (employees[i].firstName === "Theo"){
+      delete employees[i];
+    }
+    else if (employees[i].firstName === "Lorie"){
+      employees[i].department = "HR";
+    }
+  }
+   
+    return employees;
+
+}
 
 
 
@@ -68,7 +81,23 @@ var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
     3. Return the updated array.
 */
 
-//Code Here
+const removeDuplicates = function(arr){
+  
+  for(let i = 0; i < arr.length; i++){
+    
+    for (let j = arr.length - 1; j > i; j--){
+     
+      if(arr[i] === arr[j]){
+        arr.splice(j, 1);
+      }
+
+    }
+  }
+
+  return arr;
+
+}  
+
 
 
 
@@ -97,8 +126,8 @@ var cat = {
 */
 
 //Code Here
-var grumpyActivity;
-var fluffy2ndFriend;
+var grumpyActivity = cat.catFriends[0].activities[1];
+var fluffy2ndFriend = cat.catFriends[1].name;
 
 
 
@@ -138,8 +167,19 @@ var myCar = {
     3. Change atFaultForAccident from true to false.
 */
 
-//Code Here
+let recordCleaner = function(){
+ 
+  for(let i = 0; i < myCar.accidents.length; i++){
 
+      if (myCar.accidents[i].atFaultForAccident === true){
+            myCar.accidents[i].atFaultForAccident = false;
+      }
+
+  }
+  return myCar;
+}
+recordCleaner(myCar);
+console.log(myCar);
 
 
 ////////// PROBLEM 5 //////////
@@ -157,6 +197,18 @@ var numsArr = [ [1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
     4. Return the modified numsArr.
 */
 
-//Code Here
+const looper = function(){
+  for(let i = 0; i < numsArr.length; i++){
 
-
+    for(let j = 0; j < numsArr[i].length; j++){
+      if (numsArr[i][j] % 2 !== 0){
+        numsArr[i][j] = 'odd';
+      }
+      else if (numsArr[i][j] % 2 === 0){
+        numsArr[i][j] = 'even';
+      }
+    } 
+}
+  console.log(numsArr);
+  return numsArr;
+}
